d04aadfdb7d794c3c0f09871f6db40088a4c1f59
zhiwei.liu <zhiwei.liu@funplus.com>
2019-06-12 14:25:33 +0800

    增加服务器脚本

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 const gulp = require('gulp');
 const shell = require('gulp-shell');
 const browserSync = require('browser-sync').create();
-
+const del = require('del');
 
 gulp.task('build', (cb) => {
     return gulp.src('.')
@@ -24,3 +24,41 @@ gulp.task('serve', gulp.series('build', (cb) => {
     gulp.watch('./client/src/**/*.ts', callBrowserSync)
     gulp.watch('./client/resource/eui_skins/*.exml', callBrowserSync);
 }));
+
+gulp.task('copyServer', (cb)=>{
+    let destPath = './server';
+    return gulp.src('main', {cwd: destPath + '/src'})
+          .pipe(gulp.dest(destPath));
+})
+
+gulp.task('buildServer', (cb) => {
+    return gulp.src('.')
+          .pipe(shell([
+            'env GOOS=linux GOARCH=386 go build main.go'
+          ], {cwd: './server/src'}));
+});
+
+gulp.task('deleServer', (cb) => {
+    del('./server/src/main');
+    cb();
+});
+
+gulp.task('distServer', gulp.series('buildServer' , 'copyServer' , 'deleServer', (cb) => {
+    console.log("^^^^^ finish dist server");
+    return gulp.src('.')
+}));
+
+gulp.task('uploadServer', (cb) => {
+     return gulp.src('.')
+        .pipe(shell([
+          'scp main root@47.95.246.80:/root/randomLife',
+          'scp data/*.* root@47.95.246.80:/root/randomLife/data'
+        ], {cwd: './server'}));
+});
+
+gulp.task('uploadData', (cb) => {
+     return gulp.src('.')
+        .pipe(shell([
+          'scp data/*.* root@47.95.246.80:/root/randomLife/data'
+        ], {cwd: './server'}));
+});
